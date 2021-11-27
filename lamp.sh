2dcda69a0d562625015a634dd6f6522d9afd2001
CPONS <cponsdev@gmail.com>
2021-11-27 19:22:14 +0100

    update

--- lamp.sh
+++ lamp.sh
@@ -1,20 +1,18 @@
+#!/bin/bash
 # lamp.sh
 
 echo "Script d'installation automatique de LAMP (Apache2 - PHP - MariaDB - PHPMYADMIN)"
 echo "Lancez avec l'utilisateur root"
 
 
-echo "Voulez vous installation un serveur ssh (y/n)? "
-read answer
-
-if $answer = "y" ; then
+echo -n "Voulez vous installation un serveur ssh (y/n)?"
+read ouinon
+if [ "$ouinon" = "y" ] || [ "$ouinon" = "Y" ]; then
     echo "Installation de ssh"
     apt-get install openssh-server -y
     echo "Installation de openssh-server terminée"
-else
+    elif [ "$ouinon" = "n" ] || [ "$ouinon" = "N" ]; then
     echo "Installation de ssh annulée"
+else
+    echo "Il faut taper Y/N ou y/n"
 fi
-
-
-
-
