cb9e9ad68d4baec92e74e856fe2eea1b0873dd1f
CPONS <cponsdev@gmail.com>
2021-11-27 20:25:11 +0100

    update

--- lamp.sh
+++ lamp.sh
@@ -4,6 +4,7 @@
 echo "Script d'installation automatique de LAMP (Apache2 - PHP - MariaDB - PHPMYADMIN)"
 echo "Lancez avec l'utilisateur root"
 
+apt-get update
 
 echo -n "Voulez vous installation un serveur ssh (y/n)?"
 read ouinon
@@ -34,3 +35,45 @@ else
     echo "L'utilisateur root ne pourra pas se connecté en ssh"
 fi
 
+
+
+echo "Installation apache2"
+
+apt-get install -y apache2
+systemctl enable apache2
+
+ip a
+echo "Vous pouvez vous rendre sur http://ip voir si le site et fonctionnel"
+
+a2enmod rewrite
+a2enmod deflate
+a2enmod headers
+a2enmod ssl
+systemctl restart apache2
+
+
+echo "Installation de php"
+apt-get install -y php
+apt-get install -y php-pdo php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath
+nano /var/www/html/info.php
+
+echo "<?php phpinfo(); ?>" >> /var/www/html/info.php
+
+echo "Vous pouvez vous rendre sur http://ip/info.php pour voir les info php"
+
+echo "Installation de mariadb"
+apt-get install -y mariadb-server
+mariadb-secure-installation
+systemctl restart mariadb
+
+
+
+echo -n "Voulez vous installez phpmyadmin (y/n)?"
+read ouinon
+if [ "$ouinon" = "y" ] || [ "$ouinon" = "Y" ]; then
+    echo "Installation de phpmyadmin"
+    apt-get install phpmyadmin -y
+    echo "Installation de phpmyadmin terminée, rendez vous sur http://ip/phpmyadmin "
+else
+    echo "Installation de phpmyadmin annulée"
+fi
