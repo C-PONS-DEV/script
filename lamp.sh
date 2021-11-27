bffd45b03186845685bb536ccdd594606088097d
CPONS <cponsdev@gmail.com>
2021-11-27 19:52:51 +0100

    update

--- lamp.sh
+++ lamp.sh
@@ -14,3 +14,23 @@ if [ "$ouinon" = "y" ] || [ "$ouinon" = "Y" ]; then
 else
     echo "Installation de ssh annulée"
 fi
+
+echo -n "Voulez vous permettre a l'utilisateur root d'accéder au ssh (y/n)?"
+read ouinon
+if [ "$ouinon" = "y" ] || [ "$ouinon" = "Y" ]; then
+    rm /etc/ssh/sshd_config
+    echo "Include /etc/ssh/sshd_config.d/*.conf" >> /etc/ssh/sshd_config
+    echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+    echo "ChallengeResponseAuthentication no" >> /etc/ssh/sshd_config
+    echo "UsePAM yes" >> /etc/ssh/sshd_config
+    echo "X11Forwarding yes" >> /etc/ssh/sshd_config
+    echo "PrintMotd no" >> /etc/ssh/sshd_config
+    echo "AcceptEnv LANG LC_*" >> /etc/ssh/sshd_config
+    echo "Subsystem sftp /usr/lib/openssh/sftp-server" >> /etc/ssh/sshd_config
+    echo "" >> /etc/ssh/sshd_config
+    
+    echo "Configuration terminée"
+else
+    echo "L'utilisateur root ne pourra pas se connecté en ssh"
+fi
+
